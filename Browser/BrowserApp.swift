2633b672aa18e0d487d80e39196cd558ef3acf07
Samuel Ray <raysj@vcu.edu>
2025-12-21 18:31:09 -0500

    Initial Commit

---
+++ Browser/BrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowserApp.swift
+//  Browser
+//
+//  Created by Samuel Ray on 12/21/25.
+//
+
+import SwiftUI
+
+@main
+struct BrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
